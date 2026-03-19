082abe1bb08100e03a17fb877ce32f0cc3e67b98
Fernando Almaraz <fernandoalmarazdelaquintana@gmail.com>
2026-03-19 19:05:04 -0400

    fix: changing type of currency

--- shoplite-frontend/src/app/components/cart/cart.ts
+++ shoplite-frontend/src/app/components/cart/cart.ts
@@ -32,7 +32,7 @@ import { CartService } from '../../services/cart.service';
               <h3>{{ item.name }}</h3>
               <p class="item-cat">{{ item.category }}</p>
             </div>
-            <span class="item-price">\${{ item.price }}</span>
+            <span class="item-price">\Bs. {{ item.price }}</span>
             <button (click)="cartService.removeFromCart(i)" class="remove-btn" aria-label="Eliminar">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
             </button>
@@ -42,11 +42,11 @@ import { CartService } from '../../services/cart.service';
         <div class="cart-summary">
           <div class="summary-row">
             <span>Subtotal</span>
-            <span>\${{ cartService.total() }}</span>
+            <span>\Bs. {{ cartService.total() }}</span>
           </div>
           <div class="summary-total">
             <span>Total</span>
-            <span>\${{ cartService.total() }}</span>
+            <span>\Bs. {{ cartService.total() }}</span>
           </div>
           <button class="checkout-btn">Finalizar Compra</button>
         </div>
